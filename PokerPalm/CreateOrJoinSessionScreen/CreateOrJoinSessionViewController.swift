c92e5f54463edb63f4b7379a1822442e1e49cdfe
sergdort <sergdort@gmail.com>
2016-11-05 18:19:18 +0000

    Basic create session implementation

--- PokerPalm/CreateOrJoinSessionScreen/CreateOrJoinSessionViewController.swift
+++ PokerPalm/CreateOrJoinSessionScreen/CreateOrJoinSessionViewController.swift
@@ -1,10 +1,43 @@
 import UIKit
+import Domain
+import Platform
+import RxSwift
+import RxCocoa
+
+class DummyTokenGenerator: TokenGenerator {
+    func generate() -> String {
+        return UUID().uuidString
+    }
+}
 
 final class CreateOrJoinSessionViewController: UIViewController {
-    
+    let disposeBag = DisposeBag()
+
     @IBOutlet weak var createSessionButton: UIButton!
     @IBOutlet weak var joinSessionButton: UIButton!
     @IBOutlet weak var tokenTextField: UITextField!
     @IBOutlet weak var playerObserverSwitch: UISwitch!
-    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let input = CreateSessionUseCase.Input(user: User(ID: UUID(),
+                role: .player,
+                name: "Some Name"),
+                trigger: createSessionButton.rx.tap.asObservable())
+        let services = CreateSessionUseCase.Services(sessionIDGenerator: DummyTokenGenerator(),
+                repository: Repositories.sessionRepository)
+
+        CreateSessionUseCase.assemble(input: input, service: services)
+                .bindTo(rx.state)
+                .addDisposableTo(disposeBag)
+    }
+}
+
+fileprivate extension Reactive where Base: CreateOrJoinSessionViewController {
+    var state: UIBindingObserver<Base, UseCaseState<Session>> {
+        return UIBindingObserver(UIElement: base) { controller, state in
+            print(state)
+        }
+    }
 }
