cebec1d100ee3c994c27080bcaa729dc457f080f
Bohdan Orlov <bohdan.orlov@gmail.com>
2016-11-27 19:07:02 +0000

    Fix session change use case to provide start with existing session.

--- Domain/UseCases/SessionChangesUseCase.swift
+++ Domain/UseCases/SessionChangesUseCase.swift
@@ -7,6 +7,6 @@ public final class SessionChangesUseCase: UseCase {
     }
 
     public class func assemble(input session: Session, service repository: AbstractRepository<Session>) ->  Observable<Session> {
-        return repository.queryFirst(with: Session.ID == session.ID.uuidString).map { $0! }
+        return repository.queryFirst(with: Session.ID == session.ID.uuidString).map { $0! }.startWith(session)
     }
 }
